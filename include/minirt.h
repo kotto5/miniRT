2be27ed41a3cc40e035713f17e49ecf8e7f6f09a
ShotaTanemura <shota0711tane@gmail.com>
2023-04-24 18:12:35 +0900

    fix norm error

--- include/minirt.h
+++ include/minirt.h
@@ -1,8 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 18:10:42 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/04/24 18:11:21 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-typedef struct	s_data {
+typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
