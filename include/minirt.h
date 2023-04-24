278014007491df504c64cc2578a5a7100cf6badb
ShotaTanemura <shota0711tane@gmail.com>
2023-04-25 01:09:53 +0900

    add window_management

--- include/minirt.h
+++ include/minirt.h
@@ -6,12 +6,18 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:10:42 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/04/24 18:11:21 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/04/25 00:37:39 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
+
+# define WIN_WIDTH 800.00
+# define WIN_HEIGHT 600.00
+
+#include <math.h>
+#include <stdlib.h>
 
 typedef struct s_data {
 	void	*img;
@@ -20,5 +26,11 @@ typedef struct s_data {
 	int		line_length;
 	int		endian;
 }				t_data;
+
+typedef struct s_env {
+	void	*mlx;
+	void	*win;
+	t_data	img;
+}				t_env;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,35 +6,51 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 16:22:52 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/04/24 18:12:21 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/04/25 01:08:26 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include <stdbool.h>
 #include "minirt.h"
+#include "vector.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + \
+			(y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+void	init_window(void **mlx, void **win, t_data *img)
+{
+	*mlx = mlx_init();
+	*win = mlx_new_window(*mlx, WIN_WIDTH, WIN_HEIGHT, "Hello world!");
+	img->img = mlx_new_image(*mlx, WIN_WIDTH, WIN_HEIGHT);
+	img->addr = mlx_get_data_addr(img->img, &(img->bits_per_pixel), \
+									&(img->line_length), \
+									&(img->endian));
+}
+
+static void	window_management(void)
+{
+	t_env	env;
+	
+	init_window(&env.mlx, &env.win, &env.img);
+	mlx_put_image_to_window(env.mlx, env.win, env.img.img, 0, 0);
+	mlx_loop(env.mlx);
+}
+
+int	main_minirt(void)
+{
+	window_management();
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
-									&img.line_length, \
-									&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	main_minirt();
 	return (0);
 }
