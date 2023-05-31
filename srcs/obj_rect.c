e822b76c131d9b78378a77650bf7f27a7a196dcf
ShotaTanemura <shota0711tane@gmail.com>
2023-05-31 18:59:04 +0900

    fix norm errors of obj_rect.c

--- srcs/obj_rect.c
+++ srcs/obj_rect.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   obj_rect.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 15:42:42 by kakiba            #+#    #+#             */
+/*   Updated: 2023/05/31 18:58:24 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "all.h"
 
 double	get_minimum(double a, double b, double c)
 {
-	if (a <= b && a <= c )
+	if (a <= b && a <= c)
 		return (a);
 	else if (b <= a && b <= c)
 		return (b);
@@ -12,7 +24,7 @@ double	get_minimum(double a, double b, double c)
 
 double	get_maximum(double a, double b, double c)
 {
-	if (a >= b && a >= c )
+	if (a >= b && a >= c)
 		return (a);
 	else if (b >= a && b >= c)
 		return (b);
@@ -20,25 +32,21 @@ double	get_maximum(double a, double b, double c)
 		return (c);
 }
 
-
 static double	get_t_ray_rect(t_ray ray, t_rect *rect)
 {
-	double	tx_start = (rect->bound1.x - ray.pos.x) / ray.dir.x;
-	double	tx_end = (rect->bound2.x - ray.pos.x) / ray.dir.x;
-	double	ty_start = (rect->bound1.y - ray.pos.y) / ray.dir.y;
-	double	ty_end = (rect->bound2.y - ray.pos.y) / ray.dir.y;
-	double	tz_start = (rect->bound1.z - ray.pos.z) / ray.dir.z;
-	double	tz_end =(rect->bound2.z - ray.pos.z) / ray.dir.z;
+	double	start;
+	double	end;
 
-	double	start = get_maximum(tx_start, ty_start, tz_start);
-	double	end = get_minimum(tx_end, ty_end, tz_end);
-
-// 実質plane （高さ0）のケースも込み
-	if (rect->bound1.x - ray.pos.x <= 0 && rect->bound2.x - ray.pos.x >= 0 &&
-		rect->bound1.y - ray.pos.y <= 0 && rect->bound2.y - ray.pos.y >= 0 &&
+	end = get_minimum((rect->bound2.x - ray.pos.x) / ray.dir.x, \
+								(rect->bound2.y - ray.pos.y) / ray.dir.y, \
+								(rect->bound2.z - ray.pos.z) / ray.dir.z);
+	start = get_maximum((rect->bound1.x - ray.pos.x) / ray.dir.x, \
+								(rect->bound1.y - ray.pos.y) / ray.dir.y, \
+								(rect->bound1.z - ray.pos.z) / ray.dir.z);
+	if (rect->bound1.x - ray.pos.x <= 0 && rect->bound2.x - ray.pos.x >= 0 && \
+		rect->bound1.y - ray.pos.y <= 0 && rect->bound2.y - ray.pos.y >= 0 && \
 		rect->bound1.z - ray.pos.z <= 0 && rect->bound2.z - ray.pos.z >= 0)
 		return (true);
-
 	if (start >= end)
 		return (start);
 	else
@@ -47,33 +55,26 @@ static double	get_t_ray_rect(t_ray ray, t_rect *rect)
 
 t_intersection	get_intersection_rect(const t_ray ray, const t_obj *obj)
 {
-	t_rect		*rect;
+	t_rect			*rect;
 	t_intersection	intersection;
-	double	t;
-	t_vec3	dt;
-	// t_vec3	td;
-	// printf("Aa\n");
+	double			t;
+	t_vec3			dt;
 
 	rect = obj->instance;
 	intersection.does_intersect = false;
 	t = get_t_ray_rect(ray, rect);
-	// printf("t[%f] \n", t);
 	if (t < 0)
-	{
-		// printf("NOT !\n");
 		return (intersection);
-	}
 	else
 	{
-		// printf("INTER! !\n");
 		intersection.does_intersect = true;
 		return (intersection);
 	}
-
 	dt = vec_mult(ray.dir, t);
 	intersection.does_intersect = true;
 	intersection.position = vec_add(ray.pos, dt);
 	intersection.distance = vec_mag(dt);
-	intersection.vertical_dir = vec_normalize(vec_sub(intersection.position, ray.pos));
+	intersection.vertical_dir = vec_normalize(vec_sub(intersection.position, \
+														ray.pos));
 	return (intersection);
 }
