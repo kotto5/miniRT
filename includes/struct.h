776615ed71cd9c51a00949e3856f199804492f1d
ShotaTanemura <shota0711tane@gmail.com>
2023-06-06 00:53:13 +0900

    fix norm error of struct.h

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 22:24:49 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/06/05 22:50:51 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/06/06 00:51:46 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,86 @@
 # define STRUCT_H
 
 # include "all.h"
+// # include "function_pointer.h"
 
-typedef struct s_obj			t_obj;
-typedef struct s_intersection	t_intersection;
+typedef struct s_vec3
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_vec3;
+
+typedef struct s_intersection
+{
+	bool	does_intersect;
+	bool	is_inside;
+	double	distance;
+	t_vec3	position;
+	t_vec3	vertical_dir;
+}		t_intersection;
+
+typedef struct s_mat3{
+	double	m[3][3];
+}				t_mat3;
+
+typedef struct s_ray
+{
+	t_vec3	pos;
+	t_vec3	dir;
+	double	distance_to_window;
+	t_vec3	orientation;
+	t_mat3	rotation;
+}				t_ray;
+
+typedef enum e_objtype{
+	O_SPHERE,
+	O_PLANE,
+	O_CYLINDER,
+	O_RECT,
+}			t_objtype;
+
+typedef struct s_bright_color
+{
+	double	t;
+	double	r;
+	double	g;
+	double	b;
+}				t_bright_color;
+
+// Rr = Ra + Rd + Rs
+// 環境光反射係数 ambient
+// t_bright_color	am;
+// 拡散反射係数 diffuse
+// t_bright_color	di;
+// 鏡面反射係数 spqcular
+// t_bright_color	sp;
+// 1 <= shininess ハイライトの鋭さ
+// 完全鏡面反射　使うか
+typedef struct s_reflect
+{
+	t_bright_color	am;
+	t_bright_color	di;
+	t_bright_color	sp;
+	double			sp_shininess;
+	bool			use_perfect_reflectance;
+	t_bright_color	perfect_reflectance;
+}				t_reflect;
+
+struct					s_obj;
+typedef t_intersection	(*t_f_get_intersection)\
+							(const struct s_ray, const struct s_obj*);
+typedef bool			(*t_f_is_inside_point)(const struct s_obj *obj, \
+							const struct s_vec3 point);
+
+typedef struct s_obj
+{
+	t_objtype				type;
+	void					*instance;
+	t_reflect				ref;
+	t_f_get_intersection	get_intersection;
+	t_f_is_inside_point		is_inside_point;
+	bool					is_closed;
+}				t_obj;
 
 // little endian
 typedef struct s_trgb
@@ -32,34 +109,6 @@ typedef union u_img_color
 	t_trgb			trgb;
 	unsigned int	color;
 }				t_img_color;
-
-typedef struct s_bright_color
-{
-	double	t;
-	double	r;
-	double	g;
-	double	b;
-}				t_bright_color;
-
-typedef struct s_vec3
-{
-	double	x;
-	double	y;
-	double	z;
-}				t_vec3;
-
-typedef struct s_mat3{
-	double	m[3][3];
-}				t_mat3;
-
-typedef struct s_ray
-{
-	t_vec3	pos;
-	t_vec3	dir;
-	double	distance_to_window;
-	t_vec3	orientation;
-	t_mat3	rotation;
-}				t_ray;
 
 typedef struct s_img {
 	void	*img;
@@ -111,32 +160,11 @@ typedef struct s_scene
 	t_dtree			*obj_tree;
 }				t_scene;
 
-// Rr = Ra + Rd + Rs
-// 環境光反射係数 ambient
-// t_bright_color	am;
-// 拡散反射係数 diffuse
-// t_bright_color	di;
-// 鏡面反射係数 spqcular
-// t_bright_color	sp;
-// 1 <= shininess ハイライトの鋭さ
-// 完全鏡面反射　使うか
-typedef struct s_reflect
-{
-	t_bright_color	am;
-	t_bright_color	di;
-	t_bright_color	sp;
-	double			sp_shininess;
-	bool			use_perfect_reflectance;
-	t_bright_color	perfect_reflectance;
-}				t_reflect;
-
-typedef struct s_sphere			t_sphere;
-
-struct	s_sphere
+typedef struct s_sphere
 {
 	t_vec3		pos;
 	double		r;
-};
+}		t_sphere;
 
 typedef struct s_plane
 {
@@ -158,32 +186,6 @@ typedef struct s_cylinder
 	double		height;
 }				t_cylinder;
 
-typedef enum e_objtype{
-	O_SPHERE,
-	O_PLANE,
-	O_CYLINDER,
-	O_RECT,
-}			t_objtype;
-
-struct	s_intersection
-{
-	bool	does_intersect;
-	bool	is_inside;
-	double	distance;
-	t_vec3	position;
-	t_vec3	vertical_dir;
-}	t_intersection;
-
-struct	s_obj
-{
-	t_objtype		type;
-	void			*instance;
-	t_reflect		ref;
-	t_intersection	(*get_intersection)(t_ray ray, t_obj *obj);
-	bool			(*is_inside_point)(t_obj *obj, t_vec3 point);
-	bool			is_closed;
-}	t_obj;
-
 typedef enum e_lighttype{
 	L_POINT,
 	L_PARALLEL,
@@ -196,14 +198,13 @@ typedef struct s_lighting
 	t_vec3			incident_to_light;
 }				t_lighting;
 
-typedef struct s_light			t_lightsource;
-
-struct	s_light
+typedef struct s_lightsource
 {
-	t_lighttype	type;
-	void		*instance;
-	t_lighting	(*lighting_at)(t_vec3 vec, t_lightsource *light);
-};
+	t_lighttype			type;
+	void				*instance;
+	struct s_lighting	(*lighting_at)\
+							(struct s_vec3 vec, struct s_lightsource *light);
+}				t_lightsource;
 
 typedef struct s_point_light
 {
