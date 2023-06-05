b00fe1605499793ecff2211ae83d10170fefac77
ShotaTanemura <shota0711tane@gmail.com>
2023-06-05 22:55:41 +0900

    fix  norm error except for function pointers

--- includes/obj.h
+++ includes/obj.h
@@ -1,44 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   obj.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 21:52:35 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/06/05 21:54:33 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef OBJ_H
 # define OBJ_H
 
 # include "all.h"
 
-
 // new_obj.c
-t_obj		*new_obj(t_objtype type, t_reflect ref, void *obj_info);
-t_sphere	*make_sphere_instance(t_vec3 origin, double r);
-t_plane		*make_plane_instance(t_vec3 vertical, t_vec3 point);
-// t_cylinder	*make_cylinder_instance(t_vec3 vertical, t_vec3 point, double r);
-t_cylinder	*make_cylinder_instance(t_vec3 vertical, t_vec3 point, double r, double height);
-t_rect		*make_rect_instance(t_vec3 bound1, t_vec3 bound2);
-
-// t_reflect	get_t_refrect(double am, double di, double sp, double sp_shininess);
-// t_reflect	get_t_refrect(t_bright_color am, t_bright_color di, t_bright_color sp, double sp_shininess);
-t_reflect	get_t_refrect(t_bright_color am, t_bright_color di, t_bright_color sp, double sp_shininess, bool use_perfect_reflectance, t_bright_color perfect_reflectance);
-
+t_obj			*new_obj(t_objtype type, t_reflect ref, void *obj_info);
+t_sphere		*make_sphere_instance(t_vec3 origin, double r);
+t_plane			*make_plane_instance(t_vec3 vertical, t_vec3 point);
+t_cylinder		*make_cylinder_instance(t_vec3 vertical, \
+										t_vec3 point, \
+										double r, \
+										double height);
+t_rect			*make_rect_instance(t_vec3 bound1, t_vec3 bound2);
 
 // cir.c
-// double	get_ray_t_to_cir(t_ray ray, t_sphere cir);
-t_vec3	get_ray_intersect_vec(t_ray ray, double t);
-t_vec3	get_ray_intersect_vec_dir(t_ray ray, t_vec3 point);
-t_vec3	get_cir_vertical_dir(t_sphere cir, t_vec3 surface_p);
-// t_intersection	get_intersection_sphere(const t_ray ray, const t_obj *obj, t_dlist **alloc_list);
-// t_intersection	get_intersection_sphere(const t_ray ray, const t_obj *obj, t_dlist **alloc_list);
+t_vec3			get_ray_intersect_vec(t_ray ray, double t);
+t_vec3			get_ray_intersect_vec_dir(t_ray ray, t_vec3 point);
+t_vec3			get_cir_vertical_dir(t_sphere cir, t_vec3 surface_p);
 t_intersection	get_intersection_sphere(const t_ray ray, const t_obj *obj);
 t_intersection	get_intersection_plane(const t_ray ray, const t_obj *obj);
 t_intersection	get_intersection_cylinder(const t_ray ray, const t_obj *obj);
 t_intersection	get_intersection_rect(const t_ray ray, const t_obj *obj);
 
 #endif
-
-
-
-
-
-
-
-// t_obj		*new_obj(t_objtype type, t_reflect ref, void *obj_info, t_dlist **alloc_list);
-// t_sphere	*make_sphere_instance(t_vec3 origin, double r, t_dlist **gb_list);
-// t_plane		*make_plane_instance(t_vec3 vertical, t_vec3 point, t_dlist **gb_list);
-// t_cylinder	*make_cylinder_instance(t_vec3 vertical, t_vec3 point, double r, t_dlist **gb_list);
-// t_rect		*make_rect_instance(t_vec3 bound1, t_vec3 bound2, t_dlist **gb_list);
--- includes/struct.h
+++ includes/struct.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 22:24:49 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/06/05 22:50:51 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCT_H
-#define STRUCT_H
+# define STRUCT_H
 
-#include "all.h"
+# include "all.h"
 
-typedef	struct	s_obj t_obj;
-typedef struct	s_intersection t_intersection;
+typedef struct s_obj			t_obj;
+typedef struct s_intersection	t_intersection;
 
 // little endian
-typedef struct	s_trgb
+typedef struct s_trgb
 {
 	unsigned char	b;
 	unsigned char	g;
@@ -15,13 +27,13 @@ typedef struct	s_trgb
 	unsigned char	t;
 }				t_trgb;
 
-typedef union	u_img_color
+typedef union u_img_color
 {
 	t_trgb			trgb;
 	unsigned int	color;
 }				t_img_color;
 
-typedef struct	s_bright_color
+typedef struct s_bright_color
 {
 	double	t;
 	double	r;
@@ -29,18 +41,18 @@ typedef struct	s_bright_color
 	double	b;
 }				t_bright_color;
 
-typedef struct	s_vec3
+typedef struct s_vec3
 {
 	double	x;
 	double	y;
 	double	z;
 }				t_vec3;
 
-typedef struct	s_mat3{
+typedef struct s_mat3{
 	double	m[3][3];
 }				t_mat3;
 
-typedef struct	s_ray
+typedef struct s_ray
 {
 	t_vec3	pos;
 	t_vec3	dir;
@@ -49,7 +61,7 @@ typedef struct	s_ray
 	t_mat3	rotation;
 }				t_ray;
 
-typedef struct	s_img {
+typedef struct s_img {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -59,40 +71,39 @@ typedef struct	s_img {
 	int		height;
 }				t_img;
 
-typedef struct	s_mlx {
+typedef struct s_mlx {
 	void	*mlx;
 	void	*win;
 	t_img	img;
 }				t_mlx;
 
-typedef struct	s_camera
+typedef struct s_camera
 {
 	t_vec3	origin;
 	t_vec3	higher_left_corner;
 	t_vec3	horizontal;
 	t_vec3	vertical;
 	t_vec3	orientation;
-
 	t_vec3	rotation_axis;
 	double	rotation_radian;
 	double	fov;
 }				t_camera;
 
-typedef struct	s_env
+typedef struct s_env
 {
-	void	*mlx;
-	void	*win;
-	t_img	img;
-	t_ray	*eye;
-	t_dlist	*obj_list;
-	t_dlist	*light_list;
-	t_dlist	*gb_list;
-	t_dtree	*obj_tree;
+	void			*mlx;
+	void			*win;
+	t_img			img;
+	t_ray			*eye;
+	t_dlist			*obj_list;
+	t_dlist			*light_list;
+	t_dlist			*gb_list;
+	t_dtree			*obj_tree;
 	t_bright_color	*am_light;
-	t_camera	*camera;
+	t_camera		*camera;
 }				t_env;
 
-typedef struct	s_scene
+typedef struct s_scene
 {
 	t_dlist			*obj_list;
 	t_dlist			*light_list;
@@ -101,42 +112,45 @@ typedef struct	s_scene
 }				t_scene;
 
 // Rr = Ra + Rd + Rs
-typedef struct	s_reflect
+// 環境光反射係数 ambient
+// t_bright_color	am;
+// 拡散反射係数 diffuse
+// t_bright_color	di;
+// 鏡面反射係数 spqcular
+// t_bright_color	sp;
+// 1 <= shininess ハイライトの鋭さ
+// 完全鏡面反射　使うか
+typedef struct s_reflect
 {
-	// 環境光反射係数 ambient
 	t_bright_color	am;
-	// 拡散反射係数 diffuse
 	t_bright_color	di;
-	// 鏡面反射係数 spqcular
 	t_bright_color	sp;
-	// 1 <= shininess ハイライトの鋭さ
 	double			sp_shininess;
-
-	// 完全鏡面反射　使うか
 	bool			use_perfect_reflectance;
 	t_bright_color	perfect_reflectance;
 }				t_reflect;
 
-typedef	struct s_sphere t_sphere;
+typedef struct s_sphere			t_sphere;
+
 struct	s_sphere
 {
 	t_vec3		pos;
 	double		r;
 };
 
-typedef struct	s_plane
+typedef struct s_plane
 {
 	t_vec3		position;
 	t_vec3		vertical;
 }				t_plane;
 
-typedef struct	s_rect
+typedef struct s_rect
 {
 	t_vec3		bound1;
 	t_vec3		bound2;
 }				t_rect;
 
-typedef struct	s_cylinder
+typedef struct s_cylinder
 {
 	double		r;
 	t_vec3		position;
@@ -149,7 +163,7 @@ typedef enum e_objtype{
 	O_PLANE,
 	O_CYLINDER,
 	O_RECT,
-} t_objtype;
+}			t_objtype;
 
 struct	s_intersection
 {
@@ -158,58 +172,54 @@ struct	s_intersection
 	double	distance;
 	t_vec3	position;
 	t_vec3	vertical_dir;
-};
+}	t_intersection;
 
 struct	s_obj
 {
-	t_objtype	type;
-	void		*instance;
-
-	t_reflect	ref;
-	t_intersection		(*get_intersection)(const t_ray, const t_obj *);
-	bool				(*is_inside_point)(const t_obj *obj, const t_vec3 point);
-	bool				is_closed;
-};
+	t_objtype		type;
+	void			*instance;
+	t_reflect		ref;
+	t_intersection	(*get_intersection)(t_ray ray, t_obj *obj);
+	bool			(*is_inside_point)(t_obj *obj, t_vec3 point);
+	bool			is_closed;
+}	t_obj;
 
 typedef enum e_lighttype{
-	// O_LIGHT,
 	L_POINT,
 	L_PARALLEL,
-} t_lighttype;
+}				t_lighttype;
 
-typedef struct	s_lighting
+typedef struct s_lighting
 {
-	double	distance;
+	double			distance;
 	t_bright_color	intensity;
-	t_vec3	incident_to_light;
+	t_vec3			incident_to_light;
 }				t_lighting;
 
-typedef	struct s_light t_lightsource;
+typedef struct s_light			t_lightsource;
+
 struct	s_light
 {
 	t_lighttype	type;
 	void		*instance;
-	t_lighting	(*lighting_at)(t_vec3, t_lightsource *);
+	t_lighting	(*lighting_at)(t_vec3 vec, t_lightsource *light);
 };
 
-typedef struct	s_point_light
+typedef struct s_point_light
 {
 	t_vec3			pos;
 	t_bright_color	intensity;
 }				t_point_light;
 
-typedef struct	s_parallel_light
+typedef struct s_parallel_light
 {
-	t_vec3		dir;
-	// t_img_color		intensity;
+	t_vec3	dir;
 }				t_parallel_light;
 
-typedef struct	s_intersion_info
+typedef struct s_intersion_info
 {
-	t_obj	*obj;
-	t_intersection intersection;
+	t_obj			*obj;
+	t_intersection	intersection;
 }				t_intersection_info;
-
-
 
 #endif
