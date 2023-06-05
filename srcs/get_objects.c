2bb9760e33b6e1a8a251534ea8d41671dfe651bc
ShotaTanemura <shota0711tane@gmail.com>
2023-06-05 17:51:44 +0900

    fix norm error of get_objects.c

--- srcs/get_objects.c
+++ srcs/get_objects.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_objects.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kakiba <kotto555555@gmail.com>             +#+  +:+       +#+        */
+/*   By: shtanemu <shtanemu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 07:50:29 by kakiba            #+#    #+#             */
-/*   Updated: 2023/06/01 15:16:25 by kakiba           ###   ########.fr       */
+/*   Updated: 2023/06/05 17:50:59 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,11 +70,11 @@ void	*get_cylinder_node(char **split)
 
 	if (get_size_double_ptr(split) != 6)
 		return (NULL);
-	if (get_vec_from_split(split[1], &position) ||
-		get_vec_from_split(split[2], &axis) ||
-		get_ref_from_split(split[5], &ref) ||
-		ft_isdouble(split[4]) == false ||
-		ft_isdouble(split[3]) == false )
+	if (get_vec_from_split(split[1], &position) || \
+		get_vec_from_split(split[2], &axis) || \
+		get_ref_from_split(split[5], &ref) || \
+		ft_isdouble(split[4]) == false || \
+		ft_isdouble(split[3]) == false)
 		return (NULL);
 	cylinder = make_cylinder_instance(axis, position, \
 		ft_atof(split[3]), ft_atof(split[4]));
@@ -94,8 +94,8 @@ void	*get_camera(char **split)
 
 	if (get_size_double_ptr(split) != 4)
 		return (NULL);
-	if (ft_isdigit_str(split[3], 0) == false ||
-		get_vec_from_split(split[1], &position) ||
+	if (ft_isdigit_str(split[3], 0) == false || \
+		get_vec_from_split(split[1], &position) || \
 		get_vec_from_split(split[2], &orientation))
 		return (NULL);
 	camera = make_camera(ft_atof(split[3]), position, orientation);
