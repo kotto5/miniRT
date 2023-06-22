6802dfa95de8ffcb5bb196a09561218ea82e2412
ShotaTanemura <shota0711tane@gmail.com>
2023-06-22 23:51:20 +0900

    adjust fmt_checkers

--- includes/conf_validation.h
+++ includes/conf_validation.h
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 17:41:45 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/06/22 16:40:05 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/06/22 23:01:40 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 # define CONF_VALIDATION_H
 
 # define IDENTIFIERS ((char const*[]){"A", "C", "L", "pl", "sp", "cy", NULL})
-
 # define ERROR_MESSAGE_FORMAT "[Error]: miniRT: "
-# define ERROR_MESSAGE_PERROR_FORMAT "[Error]: miniRT"
 # define COLOR_STR_RED_FRONT "\x1b[31m"
 # define COLOR_STR_RED_BACK "\x1b[0m"
 
 # define ERROR_ESSENTTIAL_IDENTIFIER "Identifiers A and C are essential."
 # define ERROR_INVALID_IDENTIFIER "A given identifier is invalid."
 
-void	put_system_error(void);
+#include <stdbool.h>
+
+bool	is_valid(char *filepath);
 void	put_error(const char *message);
 
 #endif
--- srcs/conf_validation.c
+++ srcs/conf_validation.c
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 21:14:07 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/06/22 22:15:36 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/06/22 23:49:22 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,10 @@ static int	open_valid_file(const char *filepath)
 		put_error(strerror(errno));
 		exit (EXIT_FAILURE);
 	}
-	close(fd);
-	return (true);
+	return (fd);
 }
 
-static char	*get_identifier(const char *line)
+static char	*get_identifier(char *line)
 {
 	size_t	i_line;
 	char	*identifer;
@@ -48,7 +47,7 @@ static char	*get_identifier(const char *line)
 	return (identifer);
 }
 
-static bool	is_valid_identifer(const char *line)
+static bool	is_valid_identifer(char *line)
 {
 	char		*identifer;
 	size_t		i_identifiers;
@@ -95,7 +94,7 @@ static bool	has_valid_identifers(char *filepath)
 	return (true);
 }
 
-static void	set_essentials_boolean(const char *line, \
+static void	set_essentials_boolean(char *line, \
 									bool *has_ambient_light, \
 									bool *has_camera)
 {
@@ -103,9 +102,9 @@ static void	set_essentials_boolean(const char *line, \
 
 	identifer = get_identifier(line);
 	if (ft_strcmp_s(identifer, "A") == 0)
-		has_ambient_light = true;
+		*has_ambient_light = true;
 	if (ft_strcmp_s(identifer, "C") == 0)
-		has_camera = true;
+		*has_camera = true;
 	free(identifer);
 }
 
@@ -113,7 +112,6 @@ static bool	has_essental_identifers(const char *filepath)
 {
 	const int	fd = open_valid_file(filepath);
 	char		*line;
-	char		*identifer;
 	bool		has_ambient_light;
 	bool		has_camera;
 
@@ -125,7 +123,7 @@ static bool	has_essental_identifers(const char *filepath)
 		if (line == NULL)
 			break ;
 		if (ft_strcmp_s(line, "\n") != 0)
-			set_essentials_boolean(line, has_ambient_light, has_camera);
+			set_essentials_boolean(line, &has_ambient_light, &has_camera);
 		free(line);
 	}
 	close(fd);
@@ -137,13 +135,86 @@ static bool	has_essental_identifers(const char *filepath)
 	return (true);
 }
 
+static bool	fmt_checker_ambient(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	fmt_checker_camera(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	fmt_checker_light(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	fmt_checker_plane(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	fmt_checker_sphere(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	fmt_checker_cylinder(const char *line)
+{
+	printf("%s\n", line);
+	return (true);
+}
+
+static bool	has_valid_format(const char *filepath)
+{
+	const int	fd = open_valid_file(filepath);
+	char		*line;
+	char		*identifier;
+	size_t		i_identifiers;
+	static bool	(*fmt_checker[])(const char *line) = {fmt_checker_ambient, \
+														fmt_checker_camera, \
+														fmt_checker_light, \
+														fmt_checker_plane, \
+														fmt_checker_sphere, \
+														fmt_checker_cylinder};
+
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		if (ft_strcmp_s(line, "\n") != 0)
+		{
+			identifier = get_identifier(line);
+			i_identifiers = 0;
+			while (ft_strcmp_s(identifier, IDENTIFIERS[i_identifiers]))
+				i_identifiers++;
+			if (fmt_checker[i_identifiers](line) == false)
+			{
+				free(identifier);
+				free(line);
+				return (false);
+			}
+			free(identifier);
+		}
+		free(line);
+	}
+	return (true);
+}
+
 bool	is_valid(char *filepath)
 {
 	if (has_valid_identifers(filepath) == false)
 		return (false);
 	if (has_essental_identifers(filepath) == false)
 		return (false);
-	// if (has_valid_format(filepath) == false)
-	// 	return (false);
+	if (has_valid_format(filepath) == false)
+		return (false);
 	return (true);
 }
